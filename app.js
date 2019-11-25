bcb3564e1d54cd7b2c7035ef3184868412a83496
Estefan Hu <estefanhu074@gmail.com>
2019-11-25 14:53:26 -0800

    added some boiler plate

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 'use strict';
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const multer = require('multer');
+const mongoose = require('mongoose');
 
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
 app.use(express.static('public'));
 app.use(multer().none());
 
-const PORT = process.env.PORT || 8000;
-app.listen(PORT);
+app.get('/', (req, res) => {
+  res.type('text');
+  res.send('Hello World');
+})
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+})
