223cca3481e4d0af59a0d548bc1e07a960e2ec40
Estefan Hu <estefanhu074@gmail.com>
2019-11-29 22:06:00 -0800

    updated dependencies

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 'use strict';
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
 const multer = require('multer');
 
 require('dotenv').config();
 
+// EXPAND FOR AVERAGE SIZE OF POST
+app.use(bodyParser.json({ limit: '500kb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '500kb' }))
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
