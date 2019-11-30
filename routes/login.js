a4873e96e3395dafc86bbab4b574cebf18982864
Estefan Hu <estefanhu074@gmail.com>
2019-11-30 13:32:04 -0800

    refactored paths

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const path = require('path');
-
+const loginPath = __dirname + '/../public/login/';
 
 router.route('/').get((req, res) => {
-  res.sendFile(path.resolve(__dirname + '/../public/login/login.html'));
+  res.sendFile(path.resolve(loginPath + 'login.html'));
 });
 
 module.exports = router;
