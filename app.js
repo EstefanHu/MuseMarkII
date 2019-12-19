075f28d606e279a50d1e9780ffe11f3f3d911eb3
Estefan Hu <estefanhu074@gmail.com>
2019-12-19 00:30:29 -0800

    did some routing

--- app.js
+++ app.js
@@ -6,13 +6,10 @@ const session = require('express-session');
 const redisStore = require('connect-redis')(session);
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const path = require('path');
 const client = redis.createClient();
 const cors = require('cors');
 const multer = require('multer');
 const app = express();
-
-require('dotenv').config();
 
 const db = mysql.createPool({
   host: process.env.DB_URL || 'localhost',
@@ -38,10 +35,77 @@ app.use(session({
 app.use(cookieParser('secretSign#143_!223'));
 
 app.get('/', (req, res) => {
+  res.sendFile('/dashboard/dashboard.html');
+})
+
+app.post('/register', async (req, res) => {
+  let email = req.body.email;
+  let checkEmail = 'SELECT * FROM users WHERE email=?';
+  await db.query(checkEmail, email, async (err, result) => {
+    if (err) res.json({"Message": "Something went wrong with the server"});
+    if (result === 0) {
+      try {
+        let firstName = req.body.firstName;
+        let lastName = req.body.lastName;
+        let password = req.body.password;
+        let qry = 'INSERT INTO user(firstName, lastName, email, password) VALUES (?, ?, ?, ?)';
+        await db.query(qry, [firstName, lastName, email, password], (err) => {
+          if (err) {
+            res.json({"Message": "failed registration"});
+          } else {
+            res.json({"Message": "Success"});
+          }
+        });
+      } catch(err) {
+        console.log(err);
+        res.type('text').status(500).send('Something went wrong');
+      }
+    }
+  });
+});
+
+app.post('/login', async (req, res) => {
   try {
+    let email = req.body.email;
+    let password = req.body.password;
+    let qry = 'SELECT * FROM users WHERE email=? and password=?';
+    let [rows, fields] = await db.query(qry, [email, password]);
+    console.log(fields);
+    if (rows.length !== 0) {
 
+    } else {
+      
+    }
   } catch(err) {
+    res.type('text').status(500).send(err);
+  }
+});
 
+app.get('/dashboard', async (req, res) => {
+  if(!req.session.key) redirect('/');
+  try {
+    let city = req.session.city;
+    let qryPosts = 'SELECT * FROM posts WHERE city=?';
+    let qryDistricts = 'SELECT * FROM districts WHERE city=?';
+    let posts = await db.query(qryPosts, city);
+    let districts = await db.query(qryDistricts, city);
+
+    res.json({
+      "districts": districts,
+      "posts": posts
+    });
+  } catch(err) {
+    res.type('text').status(500).send(err);
+  }
+});
+
+app.get('/logout', (req, res) => {
+  if (req.session.key) {
+    req.session.destroy(() => {
+      res.redirect('/');
+    });
+  } else {
+    res.redirect('/');
   }
 });
 
